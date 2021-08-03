d8661ae926f0afdb9d4fd62a8468dd10a5f67429
daiki0730 <xcrqf313@yahoo.cp.jp>
2021-08-03 21:43:40 +0900

    KMP_DUPLICATE_LIB_OK=TRUE を設定

--- pancake_cnn_aug.py
+++ pancake_cnn_aug.py
@@ -1,3 +1,6 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
